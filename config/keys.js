ff726b9565dc8d3a94b9359346cca8ef2af34abb
Wabuluka <dwabuluka@gmail.com>
2021-11-22 11:32:56 +0300

    update

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
-// Figure out what credentials to return
-if(process.env.NODE_ENV === 'production'){
-    // In production
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
     module.exports = require('./prod');
-}else{
-    // Return development
+  } else {
+    // we are in development - return the dev keys!!!
     module.exports = require('./dev');
-}
+  }
